52ae320f574149dbd31307a305f1d04da5e84ac3
Gleidson Peres <silvamorais.ti@gmail.com>
2023-07-06 22:00:33 -0300

    refactor import categories use case and controller

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUSeCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUSeCase) {}
-
   async handle(request: Request, response: Response): Promise<Response> {
-    const all = await this.listCategoriesUseCase.execute();
-    console.log(all);
+    const listCategoriesUseCase = container.resolve(ListCategoriesUSeCase);
+
+    const all = await listCategoriesUseCase.execute();
 
     return response.json(all);
   }

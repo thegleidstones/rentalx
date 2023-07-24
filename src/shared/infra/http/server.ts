238df97492b75b0c0c53ea29c829b2b289a31811
Gleidson Peres <silvamorais.ti@gmail.com>
2023-07-24 19:03:46 -0300

    create migration to cars, and some refactors on server and database connection

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,11 +5,11 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../../container";
+import "@shared/container/";
 import { AppError } from "@shared/errors/AppError";
+import { createConnection } from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
-import { createConnection } from "../typeorm";
 import { router } from "./routes";
 
 const app = express();
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from "typeorm";
 
-import { User } from "../../../modules/accounts/infra/typeorm/entities/User";
-import { Category } from "../../../modules/cars/infra/typeorm/entities/Category";
-import { Specification } from "../../../modules/cars/infra/typeorm/entities/Specification";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { Category } from "@modules/cars/infra/typeorm/entities/Category";
+import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -13,7 +14,7 @@ const AppDataSource = new DataSource({
   database: "rentalx",
   synchronize: true,
   logging: true,
-  entities: [Category, Specification, User],
+  entities: [Car, Category, Specification, User],
   migrations: [],
   subscribers: [],
 });
